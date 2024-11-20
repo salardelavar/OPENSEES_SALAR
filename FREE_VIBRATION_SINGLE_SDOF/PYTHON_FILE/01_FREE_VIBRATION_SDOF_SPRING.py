73c819a76d74db4d79105972eb926934ea5e89c8
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2024-11-20 23:49:54 +0330

    Update 01_FREE_VIBRATION_SDOF_SPRING.py

--- FREE_VIBRATION_SINGLE_SDOF/PYTHON_FILE/01_FREE_VIBRATION_SDOF_SPRING.py
+++ FREE_VIBRATION_SINGLE_SDOF/PYTHON_FILE/01_FREE_VIBRATION_SDOF_SPRING.py
@@ -1,6 +1,6 @@
 #          #####################################################################################
 #          #                                  IN THE NAME OF ALLAH                             #
-#          #                           FREE VIBRATION ANAYSIS OF ELASTIC SPRING                #
+#          #                          FREE VIBRATION ANALYSIS OF ELASTIC SPRING                #
 #          #-----------------------------------------------------------------------------------#
 #          #              THIS PROGRAM WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)           #
 #          #                       EMAIL: salar.d.ghashghaei@gmail.com                         #
