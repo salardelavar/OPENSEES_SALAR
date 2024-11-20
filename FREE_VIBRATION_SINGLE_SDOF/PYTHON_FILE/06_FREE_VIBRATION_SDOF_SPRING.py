454c1d1944785a6d063198541990701bda4ea711
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2024-11-20 23:51:59 +0330

    Update 06_FREE_VIBRATION_SDOF_SPRING.py

--- FREE_VIBRATION_SINGLE_SDOF/PYTHON_FILE/06_FREE_VIBRATION_SDOF_SPRING.py
+++ FREE_VIBRATION_SINGLE_SDOF/PYTHON_FILE/06_FREE_VIBRATION_SDOF_SPRING.py
@@ -1,6 +1,6 @@
 #          #####################################################################################
 #          #                                  IN THE NAME OF ALLAH                             #
-#          #          FREE VIBRATION ANAYSIS OF INELASTIC SPRING WITH PULSE LOADING            #
+#          #         FREE VIBRATION ANALYSIS OF INELASTIC SPRING WITH PULSE LOADING            #
 #          #-----------------------------------------------------------------------------------#
 #          #              THIS PROGRAM WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)           #
 #          #                       EMAIL: salar.d.ghashghaei@gmail.com                         #
