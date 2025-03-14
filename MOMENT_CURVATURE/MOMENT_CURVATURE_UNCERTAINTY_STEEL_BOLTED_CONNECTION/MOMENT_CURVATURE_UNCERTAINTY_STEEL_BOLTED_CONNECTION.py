7d97ea3e06c7095f04663e35caab92fbda3ae7d4
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-03-14 17:59:31 +0330

    Update MOMENT_CURVATURE_UNCERTAINTY_STEEL_BOLTED_CONNECTION.py

--- MOMENT_CURVATURE/MOMENT_CURVATURE_UNCERTAINTY_STEEL_BOLTED_CONNECTION/MOMENT_CURVATURE_UNCERTAINTY_STEEL_BOLTED_CONNECTION.py
+++ MOMENT_CURVATURE/MOMENT_CURVATURE_UNCERTAINTY_STEEL_BOLTED_CONNECTION/MOMENT_CURVATURE_UNCERTAINTY_STEEL_BOLTED_CONNECTION.py
@@ -1,10 +1,10 @@
-##############################################################################################################
-#                                                   IN THE NAME OF ALLAH                                     #
-# MOMENT-CURVATURE ANALYSIS OF STEEL BOLTED CONNECTION SECTION IN UNCERTAINITY CONDITIONS MONTE-CARLO METHOD #
-#------------------------------------------------------------------------------------------------------------#
-#                             THIS PROGRAM WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)                     #
-#                                      EMAIL: salar.d.ghashghaei@gmail.com                                   #
-############################################################################################################## 
+######################################################################################################################
+#                                                       IN THE NAME OF ALLAH                                         #
+# PROBABILISTIC MOMENT-CURVATURE ANALYSIS OF STEEL-BOLTED CONNECTIONS UNDER UNCERTAINTY USING MONTE CARLO SIMULATION #
+#--------------------------------------------------------------------------------------------------------------------#
+#                                   THIS PROGRAM WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)                       #
+#                                             EMAIL: salar.d.ghashghaei@gmail.com                                    #
+###################################################################################################################### 
 # STEEL CONNCETION IMAGE IS GOTTEN FROM THIS PAPER:
 # Paper Author: Jayarajan Puttatt  
 # Email: puttatt1@gmail.com  
