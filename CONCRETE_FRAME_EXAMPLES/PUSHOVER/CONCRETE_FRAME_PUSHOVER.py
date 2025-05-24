85410b604f9cfd678ee1b9fe86d3e2f0bb301603
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-05-24 22:36:31 +0330

    Update CONCRETE_FRAME_PUSHOVER.py

--- CONCRETE_FRAME_EXAMPLES/PUSHOVER/CONCRETE_FRAME_PUSHOVER.py
+++ CONCRETE_FRAME_EXAMPLES/PUSHOVER/CONCRETE_FRAME_PUSHOVER.py
@@ -375,7 +375,9 @@ Omega_0 = Y[2] / Y[1]
 # Calculate Displacement Ductility Ratio (μ)
 mu = X[2] / X[1]
 # Calculate Ductility Coefficient (Rμ)
-R_mu = (2 * mu - 1) ** 0.5 / mu ** 0.5
+#R_mu = 1
+#R_mu = (2 * mu - 1)
+R_mu = mu
 # Calculate Structural Behavior Coefficient (R)
 R = Omega_0 * R_mu
 print(f'Over Strength Coefficient (Ω0):      {Omega_0:.4f}')
