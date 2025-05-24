95cad1b75565569190eb07cb6ed057d1ec13e130
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-05-24 22:34:18 +0330

    Update INELASTIC_UNCERTAINTY_SEISMIC_SDOF.py

--- UNCERTAINTY_SEISMIC_SDOF/INELASTIC_UNCERTAINTY_SEISMIC_SDOF.py
+++ UNCERTAINTY_SEISMIC_SDOF/INELASTIC_UNCERTAINTY_SEISMIC_SDOF.py
@@ -101,7 +101,9 @@ Omega_0 = fu / fy
 # Calculate Displacement Ductility Ratio (μ)
 mu = esu / ey
 # Calculate Ductility Coefficient (Rμ)
-R_mu = (2 * mu - 1) ** 0.5 / mu ** 0.5
+#R_mu = 1
+#R_mu = (2 * mu - 1)
+R_mu = mu
 # Calculate Structural Behavior Coefficient (R)
 R = Omega_0 * R_mu
 print(f'Over Strength Coefficient (Ω0):      {Omega_0[-1]:.2f}')
