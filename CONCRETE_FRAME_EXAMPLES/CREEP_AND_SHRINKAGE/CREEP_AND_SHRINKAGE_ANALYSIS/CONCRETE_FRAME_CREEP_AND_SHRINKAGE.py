023d5052ce68ad24e1f7747f26aea928aacd5108
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-07-27 17:21:13 +0330

    Update CONCRETE_FRAME_CREEP_AND_SHRINKAGE.py

--- CONCRETE_FRAME_EXAMPLES/CREEP_AND_SHRINKAGE/CREEP_AND_SHRINKAGE_ANALYSIS/CONCRETE_FRAME_CREEP_AND_SHRINKAGE.py
+++ CONCRETE_FRAME_EXAMPLES/CREEP_AND_SHRINKAGE/CREEP_AND_SHRINKAGE_ANALYSIS/CONCRETE_FRAME_CREEP_AND_SHRINKAGE.py
@@ -77,7 +77,7 @@ EtU = ftU/0.002;		# tension softening stiffness
  
 # STEEL
 # Reinforcing steel
-fy = 400            # [N/mm²] Steel Rebar Yield Strength   
+fy = 4000           # [N/mm²] Steel Rebar Yield Strength   
 Es = 2e5            # [N/mm²] Modulus of Elasticity
 ey = fy/Es          # [mm/mm] Steel Rebar Yield Strain
 fu = 1.1818*fy      # [N/mm²] Steel Rebar Ultimate Strength
