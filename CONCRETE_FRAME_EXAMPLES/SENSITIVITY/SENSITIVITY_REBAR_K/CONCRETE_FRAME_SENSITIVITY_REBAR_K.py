d98dd6bb2a41317cedbc0dce33ddbc70dabc1d4d
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-07-27 17:27:18 +0330

    Update CONCRETE_FRAME_SENSITIVITY_REBAR_K.py

--- CONCRETE_FRAME_EXAMPLES/SENSITIVITY/SENSITIVITY_REBAR_K/CONCRETE_FRAME_SENSITIVITY_REBAR_K.py
+++ CONCRETE_FRAME_EXAMPLES/SENSITIVITY/SENSITIVITY_REBAR_K/CONCRETE_FRAME_SENSITIVITY_REBAR_K.py
@@ -43,7 +43,7 @@ import PLOT_2D as S04
 # ------------------------------------------
 # STEEL
 # Reinforcing steel
-fy = 400          # [N/mm²] Steel Rebar Yield Strength   
+fy = 4000         # [N/mm²] Steel Rebar Yield Strength   
 Es = 2e5          # [N/mm²] Modulus of Elasticity
 ey = fy/Es        # [mm/mm] Steel Rebar Yield Strain
 fu = 1.1818*fy    # [N/mm²] Steel Rebar Ultimate Strength
