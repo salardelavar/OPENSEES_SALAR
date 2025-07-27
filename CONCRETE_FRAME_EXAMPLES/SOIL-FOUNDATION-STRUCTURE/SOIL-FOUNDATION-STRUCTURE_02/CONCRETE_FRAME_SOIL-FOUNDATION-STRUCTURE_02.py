1b0ed5309d042ac2fa29e359546ce849236d489d
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-07-27 17:29:34 +0330

    Update CONCRETE_FRAME_SOIL-FOUNDATION-STRUCTURE_02.py

--- CONCRETE_FRAME_EXAMPLES/SOIL-FOUNDATION-STRUCTURE/SOIL-FOUNDATION-STRUCTURE_02/CONCRETE_FRAME_SOIL-FOUNDATION-STRUCTURE_02.py
+++ CONCRETE_FRAME_EXAMPLES/SOIL-FOUNDATION-STRUCTURE/SOIL-FOUNDATION-STRUCTURE_02/CONCRETE_FRAME_SOIL-FOUNDATION-STRUCTURE_02.py
@@ -134,7 +134,7 @@ ecuU = -0.008       # [mm/mm] Concrete Compressive Ultimate Strain
  
 # STEEL
 # Reinforcing steel
-fy = 400          # [N/mm²] Steel Rebar Yield Strength   
+fy = 4000         # [N/mm²] Steel Rebar Yield Strength   
 Es = 2e5          # [N/mm²] Modulus of Elasticity
 ey = fy/Es        # [mm/mm] Steel Rebar Yield Strain
 fu = 1.1818*fy    # [N/mm²] Steel Rebar Ultimate Strength
