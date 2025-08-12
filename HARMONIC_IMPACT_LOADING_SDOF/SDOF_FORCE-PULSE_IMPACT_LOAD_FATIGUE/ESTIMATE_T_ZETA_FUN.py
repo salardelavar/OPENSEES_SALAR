960e6a9d9f2c50b3c22319af1d1b33d88e3def40
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-08-12 13:45:26 +0000

    Add files via upload
    تحلیل الاستیک و غیر الاستیک سازه یک درجه آزادی تحت اثر بار ضربه پالس گونه و محاسبه نسبت میرایی و زمان تناوب سازه در اثر پاسخ جابجایی سازه با استفاده از اوپنسیس و پایتون
    Elastic and Inelastic Analysis of SDOF Systems Under Impulsive Loading Using OpenSees and Python
    WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)

---
+++ HARMONIC_IMPACT_LOADING_SDOF/SDOF_FORCE-PULSE_IMPACT_LOAD_FATIGUE/ESTIMATE_T_ZETA_FUN.py
@@ -0,0 +1,32 @@
+# Function to estimate T and zeta
+def ESTIMATE_T_ZETA(disp, dt):
+    from scipy.signal import find_peaks
+    from scipy.optimize import fsolve
+    import numpy as np
+    n = len(disp)
+    freqs = np.fft.rfftfreq(n, d=dt)
+    spectrum = np.abs(np.fft.rfft(disp))
+    peak_idx = np.argmax(spectrum[1:]) + 1
+    fn = freqs[peak_idx]
+    T = 1.0 / fn
+
+    peaks, _ = find_peaks(np.abs(disp))
+    if len(peaks) > 1:
+        x1 = np.abs(disp[peaks[0]])
+        x2 = np.abs(disp[peaks[1]])
+        delta = np.log(x1 / x2)
+    else:
+        delta = np.nan
+        
+    # Define equation for root finding
+    def EQUATION(x, delta):
+       if np.any(x == 0):  # avoid divide by zero
+           return np.inf
+       return x**2 - 1 + ((2 * np.pi * x) / np.mean(delta)) ** 2  
+     
+    # Solve for damping ratio
+    x0 = 0.0
+    solution = fsolve(EQUATION, x0, args=(delta))   
+    zeta = solution[0]
+    
+    return T, zeta
