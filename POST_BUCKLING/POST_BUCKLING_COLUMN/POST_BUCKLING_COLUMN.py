6cd0ba1eab9f60f1f916c0798ea7293422d8c9df
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2025-12-07 00:14:47 +0330

    Update POST_BUCKLING_COLUMN.py

--- POST_BUCKLING/POST_BUCKLING_COLUMN/POST_BUCKLING_COLUMN.py
+++ POST_BUCKLING/POST_BUCKLING_COLUMN/POST_BUCKLING_COLUMN.py
@@ -3,7 +3,7 @@
 #       SIMULATION OF THE POST-BUCKLING BEHAVIOR OF A SIMPLY SUPPORTED COLUMN USING OPENSEES              #
 #                                CONSIDERING THE GEOMETRIC NONLINEARITY                                   #
 #---------------------------------------------------------------------------------------------------------#
-# IT MODELS A 2D ELASTIC BEAM-COLUMN WITH AN INITIAL IMPERFECTION AND APPLIES AN AXIAL COMPRESSIVE        #
+# IT MODELS A 2D INELASTIC BEAM-COLUMN WITH AN INITIAL IMPERFECTION AND APPLIES AN AXIAL COMPRESSIVE      #
 # LOAD TO ANALYZE LARGE DISPLACEMENTS.                                                                    #
 # 1. MODEL SETUP: A COLUMN OF LENGTH L IS DEFINED WITH N ELEMENTS, INCORPORATING A SMALL INITIAL          #
 # IMPERFECTION (HALF-SINE WAVE).                                                                          #
