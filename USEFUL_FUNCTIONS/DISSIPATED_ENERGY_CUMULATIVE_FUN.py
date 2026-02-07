00c0d3f5dcd163002a3ca42f02fbc67bad4c3a47
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2026-02-07 22:25:57 +0330

    Add files via upload
    THIS PROGRAM WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)

---
+++ USEFUL_FUNCTIONS/DISSIPATED_ENERGY_CUMULATIVE_FUN.py
@@ -0,0 +1,17 @@
+def CUMULATIVE_DISSIPATED_ENERGY_FUN(disp, force):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    disp = np.array(disp)
+    force = np.array(force)
+    cum_energy = np.zeros(len(disp))
+    for i in range(1, len(disp)):
+        d_disp = disp[i] - disp[i-1]
+        avg_force = 0.5 * (force[i] + force[i-1])
+        cum_energy[i] = cum_energy[i-1] + abs(avg_force * d_disp)
+    
+    plt.figure()
+    plt.plot(disp, cum_energy, color=COLOR)
+    plt.xlabel('Displacement')
+    plt.ylabel(TITLE)
+    plt.grid(True)
+    plt.show()
