c7cf133e0ac6bd83b89af6aeb368a3ec5087ed3f
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2026-08-16 20:14:53 +0330

    Update W(x,t)_CABLE_SIMPLY_SUPPORTED.py

--- EIGHT_ANALYSIS_DUCTILITY_DAMAGE_INDEX_&_ENERGY_DISSIPATION_CAPACITY_INDEX/04_CABLE_SIMPLY_SUPPORTED_8_ANA/W(x,t)_CABLE_SIMPLY_SUPPORTED.py
+++ EIGHT_ANALYSIS_DUCTILITY_DAMAGE_INDEX_&_ENERGY_DISSIPATION_CAPACITY_INDEX/04_CABLE_SIMPLY_SUPPORTED_8_ANA/W(x,t)_CABLE_SIMPLY_SUPPORTED.py
@@ -13,7 +13,7 @@
 #         ASSESSMENT OF DUCTILITY DAMAGE INDICES FOR STRUCTURAL ELEMENTS AND SYSTEMS AND EVALUATION       #
 #                                        OF ENERGY DISSIPATION CAPACITY INDEX                             #
 #---------------------------------------------------------------------------------------------------------#
-#                    THIS PYTHON IS SCRIPT WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)                  #
+#                    THIS PYTHON SCRIPT IS WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)                  #
 #                                   EMAIL: salar.d.ghashghaei@gmail.com                                   #
 ###########################################################################################################
 """
