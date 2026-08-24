26a1dae00b3c908b59379ad4081c238767a607dc
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2026-08-24 15:21:24 +0330

    Add files via upload
    THIS PYTHON SCRIPT IS WRITTEN BY SALAR DELAVAR GHASHGHAEI (QASHQAI)

---
+++ EIGHT_ANALYSIS_DUCTILITY_DAMAGE_INDEX_&_ENERGY_DISSIPATION_CAPACITY_INDEX/22_MDOF_8_ANA/+22_MDOF_8_ANA/PERIOD_FUN.py
@@ -0,0 +1,15 @@
+def PERIOD_FUN(disp, dt):
+    from scipy.fft import fft, fftfreq
+    import numpy as np
+    # --- FFT to find dominant frequency ---
+    N = len(disp)
+    yf = fft(disp)
+    xf = fftfreq(N, dt)[:N//2]
+    
+    # Find peak frequency in positive frequencies
+    idx_peak = np.argmax(np.abs(yf[:N//2]))
+    freq_peak = xf[idx_peak]
+    period = 1.0 / freq_peak
+    
+    print(f"\nEstimated Period from FFT: {period:.4f} [s]\n\n")
+    return period
